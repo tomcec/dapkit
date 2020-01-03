dad023aa5c8a1a62ff61c0edac3f488a726515e3
Iurii Shchekochikhin <iurii.shchekochikhin@broadcom.com>
2020-01-03 17:46:12 +0100

    Manage to read content size

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,34 @@ use json::{array, object};
 use std::io::prelude::*;
 use std::net::{TcpListener, TcpStream};
 
-fn read_request(stream: &mut TcpStream) -> dap::DapMessage {
-    let mut msg: String = String::new();
-    stream.set_read_timeout(Some(std::time::Duration::new(1, 0)));
-    stream.read_to_string(&mut msg);
-    dap::DapMessage {
-        header: 3,
-        content: msg
+fn read_header(stream: &mut TcpStream) -> Result<i64, std::io::Error> {
+    let mut done = false;
+    let mut header_bytes: Vec<u8> = Vec::new();
+    while !done {
+        let mut ch: [u8; 1] = [0];
+        stream.read(&mut ch[..])?;
+        if header_bytes.last() == Some(&b'\r') && ch[0] == b'\n' {
+            done = true;
+        }
+        header_bytes.push(ch[0]);
     }
+    let content_len = String::from_utf8(header_bytes).unwrap();
+    if content_len.starts_with("Content-Length:") {
+        let num = content_len["Content-Length:".len()..].trim();
+        return Ok(num.parse::<i64>().unwrap());
+    }
+    Err(std::io::Error::new(std::io::ErrorKind::Other, "Wrong header"))
+}
+
+fn read_request(stream: &mut TcpStream) -> Result<dap::DapMessage, std::io::Error> {
+    let mut msg: String = String::new();
+    stream.set_read_timeout(Some(std::time::Duration::new(10, 0)))?;
+    let header: i64 = read_header(stream)?;
+    // stream.read_to_string(&mut msg)?;
+    Ok(dap::DapMessage {
+        header: header,
+        content: msg,
+    })
 }
 
 fn main() -> std::io::Result<()> {
@@ -30,12 +50,14 @@ fn main() -> std::io::Result<()> {
     let json_str = json::stringify(json);
     for stream in listener.incoming() {
         let mut io = stream?;
-        let msg: dap::DapMessage = read_request(&mut io);
+        let msg: dap::DapMessage = read_request(&mut io)?;
         io.write_all(
             json::stringify(object! {
                 "header" => msg.header,
                 "content" => msg.content
-            }).as_bytes());
+            })
+            .as_bytes(),
+        );
     }
     Ok(())
 }
