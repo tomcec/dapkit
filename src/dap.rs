80c377a7effe5a56725325a251a785f647924912
Iurii Shchekochikhin <iurii.shchekochikhin@broadcom.com>
2020-02-13 21:11:01 +0100

    small refactoring

--- src/dap.rs
+++ src/dap.rs
@@ -1,4 +1,39 @@
+use std::io::prelude::*;
+
 pub struct DapMessage {
     pub header: i64,
     pub content: String,
+}
+
+fn read_header(stream: &mut dyn Read) -> Result<i64, std::io::Error> {
+    let mut done = false;
+    let mut header_bytes: Vec<u8> = Vec::new();
+    while !done {
+        let mut ch: [u8; 1] = [0];
+        stream.read(&mut ch[..])?;
+        if header_bytes.last() == Some(&b'\r') && ch[0] == b'\n' {
+            done = true;
+        }
+        header_bytes.push(ch[0]);
+    }
+    let content_len = String::from_utf8(header_bytes).unwrap();
+    if content_len.starts_with("Content-Length:") {
+        let num = content_len["Content-Length:".len()..].trim();
+        return Ok(num.parse::<i64>().unwrap());
+    }
+    Err(std::io::Error::new(
+        std::io::ErrorKind::Other,
+        "Wrong header",
+    ))
+}
+
+pub fn read_message(stream: &mut dyn Read) -> Result<DapMessage, std::io::Error> {
+    let header: i64 = read_header(stream)?;
+    let mut buf = vec![0u8; header as usize];
+    stream.read_exact(&mut buf)?;
+    let msg: String = String::from_utf8(buf).unwrap();
+    Ok(DapMessage {
+        header: header,
+        content: msg,
+    })
 }
--- src/main.rs
+++ src/main.rs
@@ -2,42 +2,8 @@ mod dap;
 mod script;
 use json::object;
 use std::io::prelude::*;
-use std::net::{TcpListener, TcpStream};
+use std::net::TcpListener;
 use script::*;
-
-fn read_header(stream: &mut TcpStream) -> Result<i64, std::io::Error> {
-    let mut done = false;
-    let mut header_bytes: Vec<u8> = Vec::new();
-    while !done {
-        let mut ch: [u8; 1] = [0];
-        stream.read(&mut ch[..])?;
-        if header_bytes.last() == Some(&b'\r') && ch[0] == b'\n' {
-            done = true;
-        }
-        header_bytes.push(ch[0]);
-    }
-    let content_len = String::from_utf8(header_bytes).unwrap();
-    if content_len.starts_with("Content-Length:") {
-        let num = content_len["Content-Length:".len()..].trim();
-        return Ok(num.parse::<i64>().unwrap());
-    }
-    Err(std::io::Error::new(
-        std::io::ErrorKind::Other,
-        "Wrong header",
-    ))
-}
-
-fn read_message(stream: &mut TcpStream) -> Result<dap::DapMessage, std::io::Error> {
-    let header: i64 = read_header(stream)?;
-    let mut buf = vec![0u8; header as usize];
-    stream.read_exact(&mut buf)?;
-    let msg: String = String::from_utf8(buf).unwrap();
-    Ok(dap::DapMessage {
-        header: header,
-        content: msg,
-    })
-}
-
 
 fn main() -> std::io::Result<()> {
     let script = load_script("session_01.dap")?;
@@ -55,7 +21,7 @@ fn main() -> std::io::Result<()> {
         // 3.1 If no response found - stop
         // 4. goto 1
 
-        let msg: dap::DapMessage = read_message(&mut io)?;
+        let msg: dap::DapMessage = dap::read_message(&mut io)?;
         io.write_all(
             json::stringify(object! {
                 "header" => msg.header,
