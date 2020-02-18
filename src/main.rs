1a6e0365510d22b0b7a0de4f787a082d16b54fac
Iurii Shchekochikhin <iurii.shchekochikhin@broadcom.com>
2020-02-18 01:30:31 +0100

    introduce run_script method

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,8 @@
-mod dap;
 mod script;
+mod dap;
 use clap::Clap;
-use json::object;
 use script::*;
-use std::io::prelude::*;
+
 use std::net::TcpListener;
 
 #[derive(Clap)]
@@ -24,21 +23,7 @@ fn main() -> std::io::Result<()> {
     for stream in listener.incoming() {
         let mut io = stream?;
         io.set_read_timeout(Some(std::time::Duration::new(10, 0)))?;
-
-        // 1. if script says send something - do it now.
-        // 2. Wait for message
-        // 3. Match message to expected in script
-        // 3.1 If no response found - stop
-        // 4. goto 1
-
-        let msg: dap::DapMessage = dap::read_message(&mut io)?;
-        io.write_all(
-            json::stringify(object! {
-                "header" => msg.header,
-                "content" => msg.content
-            })
-            .as_bytes(),
-        )?;
+        script.run_script(&mut io);
     }
     Ok(())
 }
--- src/script.rs
+++ src/script.rs
@@ -1,3 +1,7 @@
+use crate::dap;
+use json::object;
+use std::io::prelude::*;
+
 #[derive(Debug)]
 pub enum Peers {
     Ide,
@@ -24,15 +28,36 @@ pub fn load_script(filename: &str) -> Result<DAPScript, std::io::Error> {
         let source: Peers = match act["source"].as_str() {
             Some("ide") => Peers::Ide,
             Some("da") => Peers::Da,
-            _ => panic!("source missing")
+            _ => panic!("source missing"),
         };
 
         interaction.push(ScriptInteraction {
             source: source,
-            content: act["content"].dump()
+            content: act["content"].dump(),
         });
     }
     return Ok(DAPScript {
         interactions: interaction,
     });
+}
+
+impl DAPScript {
+    pub fn run_script<T: Read + Write>(&self, stream: &mut T) {
+        // 1. if script says send something - do it now.
+        // 2. Wait for message
+        // 3. Match message to expected in script
+        // 3.1 If no response found - stop
+        // 4. goto 1
+    
+        let msg: dap::DapMessage = dap::read_message(stream).unwrap();
+        stream
+            .write_all(
+                json::stringify(object! {
+                    "header" => msg.header,
+                    "content" => msg.content
+                })
+                .as_bytes(),
+            )
+            .unwrap();
+    }
 }
